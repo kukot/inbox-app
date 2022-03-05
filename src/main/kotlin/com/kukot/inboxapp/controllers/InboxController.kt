3dff6d53edf6d7fe1744286cd9018ced3f2e1928
kukot <phucnn.work@gmail.com>
2022-03-05 22:44:43 +0700

    linking folder selection

--- src/main/kotlin/com/kukot/inboxapp/controllers/InboxController.kt
+++ src/main/kotlin/com/kukot/inboxapp/controllers/InboxController.kt
@@ -1,6 +1,7 @@
 package com.kukot.inboxapp.controllers
 
 import com.datastax.oss.driver.api.core.uuid.Uuids
+import com.kukot.inboxapp.constants.AppConstants
 import com.kukot.inboxapp.emaillist.EmailListItem
 import com.kukot.inboxapp.emaillist.EmailListItemRepository
 import org.ocpsoft.prettytime.PrettyTime
@@ -9,6 +10,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import java.util.Date
 
 @Controller
@@ -17,11 +19,11 @@ class InboxController(
 ) {
 
     @GetMapping
-    fun viewHomePage(model: Model, @AuthenticationPrincipal principal: OAuth2User?): String {
+    fun viewHomePage(@RequestParam("folder", required = false) folder: String?, model: Model, @AuthenticationPrincipal principal: OAuth2User?): String {
         val loginId = principal?.getAttribute<String>("login")
         if (loginId != null) {
             val inboxEmailList: List<EmailListItem> =
-                emailListItemRepository.findAllByKey_IdAndKey_Label(loginId, "Inbox")
+                emailListItemRepository.findAllByKey_IdAndKey_Label(loginId, folder ?: AppConstants.INBOX_FOLDER_LABEL)
             val prettyTime = PrettyTime()
             inboxEmailList.forEach {
                 val sentDate = Uuids.unixTimestamp(it.key.timeUUID)
